4ebcb613448c440967bad0068891b02eaaa4d3b0
Mukrosz <6380481+Mukrosz@users.noreply.github.com>
2025-05-30 17:14:39 -0600

    Refined Config widget

--- widgets/config_widget.py
+++ widgets/config_widget.py
@@ -6,7 +6,7 @@ import platform
 
 class ConfigWidget(Static):
     def __init__(self, title: str, config: dict):
-        super().__init__(classes="widget-base")
+        super().__init__(classes='widget-base')
         self.border_title = title
         self.config = config
 
@@ -14,18 +14,18 @@ class ConfigWidget(Static):
         self.refresh_display()
 
     def refresh_display(self):
-        sync_hash = self.config.get("syncHash", "N/A")
-        key = self.config.get("key", "N/A")
-        wallet = self.config.get("wallet", "N/A")
-        hostname = socket.gethostname()
+        sync_hash   = self.config.get('syncHash', 'N/A')
+        key         = self.config.get('key', 'N/A')
+        wallet      = self.config.get('wallet', 'N/A')
+        hostname    = socket.gethostname()
         os_platform = platform.architecture()
 
         content = (
-            f"Sync Name: {sync_hash}\n"
-            f"Sync Key: {key}\n"
-            f"Wallet: {wallet}\n"
-            f"Hostname: {hostname}\n"
-            f"Platform: {os_platform}"
+            f"Sync Name : {sync_hash}\n"
+            f"Sync Key  : {key}\n"
+            f"Wallet    : {wallet}\n"
+            f"Hostname  : {hostname}\n"
+            f"Platform  : {os_platform}\n"
         )
         self.update(content)
 
